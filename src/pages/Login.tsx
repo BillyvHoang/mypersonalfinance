b62f76c378bdb645b8954db5e8662bb25ebb9280
vnguyen5792 <vietaddisonguyen@gmail.com>
2023-12-31 14:36:17 -0600

    Setting up the base elements of the website: text, text boxes, buttons, anchor tag, and image

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,12 +4,20 @@ function Login(){
         <div className="flex flex-col h-screen w-screen bg-background sm:flex-row">
             {/* Login  Container*/}
             <div className="container bg-Third h-1/2 sm:h-screen">
-                hello
+                <div className="container h-screen flex justify-center content-center flex-col">
+                    <h1 className="text-3xl text-white">Welcome Back</h1>
+                    <p className="text-white">We're here to help control your finance</p>
+                    <input type="text" placeholder="Email" className="input input-bordered w-full max-w-xs bg-transparent" />
+                    <input type="text" placeholder="Password" className="input input-bordered w-full max-w-xs bg-transparent" />
+                    <a className="text-white" href="">Forgot password?</a>
+                    <button className="btn bg-secondary text-white">Login</button>
+                    <button className="btn bg-primary text-white">Sign Up</button>
+                </div>
             </div>
 
             {/* Image Container */}
-            <div className="container">
-                <img ></img>
+            <div className="container flex justify-center content-center">
+                <img src="/assets/imageDisplay.png" alt="Person Using Phone Sitting in Chair and Money in Air" />
             </div>
         </div>
     )
